77fb41f39c7d5683dc4d59729bad17ee037f1bf8
suyeon <suyeon2109@gmail.com>
2024-05-14 16:26:34 +0900

    feature. 답변 리스트 조회, 상세 조회 api 구현

--- src/main/java/com/question/memo/controller/AnswerController.java
+++ src/main/java/com/question/memo/controller/AnswerController.java
@@ -1,13 +1,21 @@
 package com.question.memo.controller;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.question.memo.domain.Answer;
 import com.question.memo.dto.CommonResponse;
 import com.question.memo.dto.answer.AnswerRequestDto;
+import com.question.memo.dto.answer.AnswerResponseDto;
 import com.question.memo.service.AnswerService;
 
 import jakarta.validation.Valid;
@@ -15,9 +23,11 @@ import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/answers")
 public class AnswerController {
 	private final AnswerService answerService;
-	@PostMapping("/answers")
+
+	@PostMapping
 	public CommonResponse<String> saveAnswer(@Valid @RequestBody AnswerRequestDto request, BindingResult e) {
 		if (e.hasErrors()) {
 			throw new IllegalArgumentException(e.getFieldErrors().get(0).getField() + " is null");
@@ -27,6 +37,31 @@ public class AnswerController {
 		CommonResponse<String> response = CommonResponse.<String>builder()
 			.code(HttpStatus.OK.value())
 			.message("답변을 저장했습니다.")
+			.build();
+		return response;
+	}
+
+	@GetMapping
+	public CommonResponse<List<AnswerResponseDto>> getAnswerList(String memberId) {
+		String request = Optional.ofNullable(memberId).orElseThrow(() -> new IllegalArgumentException("memberId is null"));
+		List<AnswerResponseDto> list = answerService.getAnswerList(request);
+
+		CommonResponse<List<AnswerResponseDto>> response = CommonResponse.<List<AnswerResponseDto>>builder()
+			.code(HttpStatus.OK.value())
+			.message("답변 리스트 조회")
+			.payload(list)
+			.build();
+		return response;
+	}
+
+	@GetMapping("/{answerSeq}")
+	public CommonResponse<AnswerResponseDto> getAnswer(@PathVariable Long answerSeq) {
+		AnswerResponseDto dto = answerService.getAnswer(answerSeq);
+
+		CommonResponse<AnswerResponseDto> response = CommonResponse.<AnswerResponseDto>builder()
+			.code(HttpStatus.OK.value())
+			.message("답변 상세 조회")
+			.payload(dto)
 			.build();
 		return response;
 	}
